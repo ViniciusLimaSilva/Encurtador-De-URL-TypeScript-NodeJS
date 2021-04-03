af1ec4409f1fceb49dc73dd723c25cdc559a73eb
Vinicius Antônio <45276630+ViniciusLimaSilva@users.noreply.github.com>
2021-04-03 01:10:37 -0300

    Delete Constants.ts

--- src/config/Constants.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  API_URL: 'http://localhost:3000',
-  MONGO_CONECTION:
-    'mongodb+srv://ViniciusLimaDB:<Password>@<database>.kmapi.mongodb.net/<database>?retryWrites=true&w=majority',
-};
